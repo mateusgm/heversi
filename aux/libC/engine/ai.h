27b88b27349f4a7ae8f90b722c9ca4f8586d7d5d
Mateus Galvão <mateus@uololou.com.br>
2011-04-23 16:42:50 -0300

    Removing the auxiliary Haskell packages from the repo.

--- aux/libC/engine/ai.h
+++
@@ -1,9 +0,0 @@
-#ifndef TKK_AS_C_AI_H
-#define TKK_AS_C_AI_H
-
-#include "game.h"
-
-/** Play as player, using artificial intelligence. Returns the player's move. **/
-struct Coord aiPlay(struct Game const* game);
-
-#endif
--- aux/reversi-1.0-src/Compilar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-path c:\ghc\ghc-6.4\bin;%PATH%
-
-ghc --make -package wx Reversi.hs -o Reversi.exe
-
-del *.hi
-del *.o
-
-pause
